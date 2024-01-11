afa49d07d580fd18e8fbd23b97247da18a693f8e
pratham1324098 <94662966+pratham1324098@users.noreply.github.com>
2024-01-11 13:04:57 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import {getStorage} from 'firebase/storage'
 import {getFirestore} from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "AIzaSyBqLWtVFfQOJkEtSc3gDoc0Ld1XJYRBdrY",
-  authDomain: "chatting-app-6b35b.firebaseapp.com",
-  projectId: "chatting-app-6b35b",
-  storageBucket: "chatting-app-6b35b.appspot.com",
-  messagingSenderId: "398560408679",
-  appId: "1:398560408679:web:3d0f697a2fda43cd3b10c2",
-  measurementId: "G-1LHPVBJC4B"
+  apiKey: "",
+  authDomain: "chatting-app-.firebaseapp.com",
+  projectId: "chatting-app-",
+  storageBucket: "chatting-app-.appspot.com",
+  messagingSenderId: "",
+  appId: "1:398560408679:web:",
+  measurementId: "G-"
 };
 
 export const app = initializeApp(firebaseConfig);
